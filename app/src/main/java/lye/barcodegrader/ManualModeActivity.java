324f2a896b666e2bdfef0944b7e4888b285fb8b6
Julio Domingo Jiménez Ledesma <i72jilej@uco.es>
2016-04-11 16:13:45 +0200

    Terminando el modo manual (Mirar TODOs y FIXMEs)

--- app/src/main/java/lye/barcodegrader/ManualModeActivity.java
+++ app/src/main/java/lye/barcodegrader/ManualModeActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,7 +24,7 @@ public class ManualModeActivity extends AppCompatActivity {
     TextView codigoAlumno;
     TextView notaActual;
     TextView notaMax;
-    TextView fechaEdicion;
+    //TextView fechaEdicion;
 
     int filaAlumno;
 
@@ -36,15 +37,27 @@ public class ManualModeActivity extends AppCompatActivity {
         nombreAlumno = (TextView) findViewById(R.id.nombreAlumno);
         codigoAlumno = (TextView) findViewById(R.id.codigoAlumno);
         notaActual = (TextView) findViewById(R.id.notaAlumno);
-        notaMax = (TextView) findViewById(R.id.notaMax);
-        fechaEdicion = (TextView) findViewById(R.id.fechaEdicion);
-
-
+        notaMax = (TextView) findViewById(R.id.notaMaxima);
+        //fechaEdicion = (TextView) findViewById(R.id.fechaEdicion);
 
         //Recibiendo intent
         csvArray2 = (ArrayList<String[]>) getIntent().getSerializableExtra(MainActivity.EXTRA_MESSAGE_2);
 
-        //notaMax.setText(csvArray2.get(1)[5]);
+        notaMax.setText(csvArray2.get(1)[5]);
+
+        findViewById(R.id.nota0).setOnClickListener(mGlobal_OnClickListener);
+        findViewById(R.id.nota1).setOnClickListener(mGlobal_OnClickListener);
+        findViewById(R.id.nota2).setOnClickListener(mGlobal_OnClickListener);
+        findViewById(R.id.nota3).setOnClickListener(mGlobal_OnClickListener);
+        findViewById(R.id.nota4).setOnClickListener(mGlobal_OnClickListener);
+        findViewById(R.id.nota5).setOnClickListener(mGlobal_OnClickListener);
+        findViewById(R.id.nota6).setOnClickListener(mGlobal_OnClickListener);
+        findViewById(R.id.nota7).setOnClickListener(mGlobal_OnClickListener);
+        findViewById(R.id.nota8).setOnClickListener(mGlobal_OnClickListener);
+        findViewById(R.id.nota9).setOnClickListener(mGlobal_OnClickListener);
+        findViewById(R.id.nota10).setOnClickListener(mGlobal_OnClickListener);
+        findViewById(R.id.notaBorrar).setOnClickListener(mGlobal_OnClickListener);
+
 
         /*
         //TODO TEST (BORRAR LUEGO)
@@ -97,6 +110,19 @@ public class ManualModeActivity extends AppCompatActivity {
                 filaAlumno = buscarAlumno(scanContent);
                 //TODO Comprobar si se ha devuelto que el alumno no existe
                 //TODO Recoger datos del alumno
+                if (filaAlumno == 0) {
+                    Toast.makeText(getApplicationContext(), "Alumno no encontrado en el fichero cargado", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    nombreAlumno.setText(csvArray2.get(filaAlumno)[1]);
+                    codigoAlumno.setText(csvArray2.get(filaAlumno)[2].replace("@uco.es",""));
+                    if (csvArray2.get(filaAlumno)[4].equals("")) {
+                        notaActual.setText("-");
+                    }
+                    else {
+                        notaActual.setText(csvArray2.get(filaAlumno)[4]);
+                    }
+                }
 
                 //System.out.println(filaAlumno);
 
@@ -116,8 +142,7 @@ public class ManualModeActivity extends AppCompatActivity {
         String correo = codigo + "@uco.es";
 
         while (fila < csvArray2.size() && !csvArray2.get(fila)[2].equals(correo)) {
-            System.out.println(fila + " - " + csvArray2.get(fila)[2] + " =? " + correo + "\n\r");
-            System.out.println(csvArray2.get(fila)[2].length() + " - " + correo.length() + "\n\r");
+            //System.out.println(fila + " - " + csvArray2.get(fila)[2] + " =? " + correo + "\n\r");
             fila++;
         }
 
@@ -127,6 +152,47 @@ public class ManualModeActivity extends AppCompatActivity {
 
         return fila;
     }
+
+    //Global On click listener for all views
+    final View.OnClickListener mGlobal_OnClickListener = new View.OnClickListener() {
+        public void onClick(final View v) {
+            Button boton = (Button) findViewById(v.getId());
+            //Toast.makeText(getApplicationContext(), boton.getText(), Toast.LENGTH_SHORT).show();
+            String notaPantalla;
+            String notaCSV;
+
+            //TODO Comprobar si hay un alumno cargado
+            if (boton.getText() == "Borrar") {
+                //FIXME Al elegir borrar, aparece "Borrar" en pantalla
+                notaPantalla = "-";
+                notaCSV = "";
+            } else {
+                notaPantalla = boton.getText().toString();
+                notaCSV = notaPantalla;
+            }
+
+            notaActual.setText(notaPantalla);
+            csvArray2.get(filaAlumno)[4] = notaCSV;
+
+            /*switch(v.getId()) {
+
+
+                case R.id.nota0:
+                    //Inform the user the button1 has been clicked
+                    Toast.makeText(getApplicationContext(), "Nota0 click", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.nota1:
+                    //Inform the user the button2 has been clicked
+                    Toast.makeText(getApplicationContext(), "Nota1 click.", Toast.LENGTH_SHORT).show();
+                    break;
+            }*/
+            System.out.println("COMUN");
+        }
+    };
+
+
+
+
 
     /*
     //TODO TEST (BORRAR LUEGO)
